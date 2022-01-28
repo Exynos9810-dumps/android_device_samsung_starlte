f400876b2f70d5fdab0be8037a10f83c3755fe13
baddar90 <mnbaddar1990@gmail.com>
2022-01-28 21:47:51 +0400

    add stock manifests

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,5 +25,17 @@ TARGET_KERNEL_CONFIG := exynos9810-starlte_defconfig
 # Partitions
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 4561305600
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.cas@1.1-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vaultkeeper_manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vendor.samsung.hardware.authfw@1.0-manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vendor.samsung.hardware.base@1.0-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vendor.samsung.hardware.tlc.atn@1.0-manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vendor.samsung.hardware.tlc.iccc@1.0-manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vendor.samsung.hardware.tlc.tima@1.0-manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
 # Inherit common board flags
 include device/samsung/universal9810-common/BoardConfigCommon.mk
